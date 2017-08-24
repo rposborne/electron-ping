959138bf99a8dafdaa6e78d20c8c5872da813867
Russell Osborne <russell@theironyard.com>
2017-08-23 21:05:16 -0400

    Implement app icon change handler

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const {app, Menu, Tray} = require('electron')
+const {app, Menu, Tray, MenuItem} = require('electron')
 
 let appIcon = null
 app.on('ready', () => {
   appIcon = new Tray('images/tray_icon_purple.png')
   const contextMenu = Menu.buildFromTemplate([
-    {label: 'Item1', type: 'radio'},
-    {label: 'Item2', type: 'radio'}
+    new MenuItem({
+      label: 'MenuItem1', 
+      click() {
+        console.log(Object.keys(appIcon), "appIcon")
+        appIcon.setImage('images/tray_icon_black.png')
+        appIcon.setImage('images/tray_icon_purple.png')
+        console.log('item 1 clicked')
+      }
+    }),
+    {type: 'separator'},
+    {label: 'Quit the greatest app ever?', role: 'quit'}
   ])
-
   // Make a change to the context menu
   contextMenu.items[1].checked = false
 
